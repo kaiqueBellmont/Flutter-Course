eca2b766d639b7861542f4d7b3747dcc91dbbc41
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-11-02 19:46:25 -0300

    second commit

---
+++ questions_project/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
