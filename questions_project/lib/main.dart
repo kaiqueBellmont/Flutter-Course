a476de6f5a66b1e16d426091bfc351048694acfc
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-11-03 13:49:02 -0300

    third commit

--- questions_project/lib/main.dart
+++ questions_project/lib/main.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const PerguntaApp());
+  runApp(PerguntasApp());
 }
 
-class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({super.key});
+class PerguntasApp extends StatefulWidget {
+  @override
+  _PerguntasAppState createState() => _PerguntasAppState();
+}
+
+class _PerguntasAppState extends State<PerguntasApp> {
+  int perguntaIndex = 0;
+
+  void responder() {
+    print("Pergunta Respondida !");
+  }
+
+  final List<String> perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Perguntas'),
+          title: const Text("Perguntas"),
         ),
         body: Column(
-          children: [
+          children: <Widget>[
             Text(perguntas[0]),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('Resposta 1'),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text("Resposta 1"),
             ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('Resposta 2'),
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text('Resposta 3'),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text("Resposta 2"),
             ),
           ],
         ),
