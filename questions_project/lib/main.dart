1b213fae26ac6669004a8d620c66fe447462410c
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-11-10 19:53:17 -0300

    exercises 43

--- questions_project/lib/main.dart
+++ questions_project/lib/main.dart
@@ -1,47 +1,57 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
-void main() {
-  runApp(PerguntasApp());
-}
+void main() => runApp(const PerguntaApp());
 
-class PerguntasApp extends StatefulWidget {
-  @override
-  _PerguntasAppState createState() => _PerguntasAppState();
-}
-
-class _PerguntasAppState extends State<PerguntasApp> {
-  int perguntaIndex = 0;
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
 
   void responder() {
-    print("Pergunta Respondida !");
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
   }
-
-  final List<String> perguntas = [
-    'Qual é a sua cor favorita?',
-    'Qual é o seu animal favorito?'
-  ];
 
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Perguntas"),
+          title: const Text('Perguntas'),
         ),
         body: Column(
-          children: <Widget>[
-            Text(perguntas[0]),
+          children: [
+            Questao(perguntas[perguntaSelecionada]),
             ElevatedButton(
               onPressed: responder,
-              child: const Text("Resposta 1"),
+              child: const Text('Resposta 1'),
             ),
             ElevatedButton(
               onPressed: responder,
-              child: const Text("Resposta 2"),
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 3'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
